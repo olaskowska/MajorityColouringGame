7f53755aeccd142643a6c6080e5c9889c6f5e8e9
olaskowska <92526318+olaskowska@users.noreply.github.com>
2023-08-11 13:37:48 +0200

    deleting unnecessary imports

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 import Game from "./Game.js";
-import Level from "./Level.js";
-import Vertex from "./Vertex.js";
 
 export const green = "#6FFA6D"; //color used by user
 export const red = "#FF3232"; //color used by a computer
